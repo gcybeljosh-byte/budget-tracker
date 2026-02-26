9504685ba445457e6a44df5e36e6d9ce33187d0b
Cybel Josh A. Gamido <gcybeljosh@gmail.com>
2026-02-26 14:19:46 +0800

    Update journal.php

--- core/journal.php
+++ core/journal.php
@@ -9,8 +9,9 @@ include '../includes/header.php';
 
 <?php include '../includes/sidebar.php'; ?>
 
-<!-- Select2 CSS -->
+<!-- Select2 CSS & Bootstrap 5 Theme -->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
 <style>
     .bg-orange {
@@ -1136,6 +1137,7 @@ include '../includes/header.php';
         $('.select2-tags').each(function() {
             var $this = $(this);
             $this.select2({
+                theme: 'bootstrap-5',
                 placeholder: "Select tags...",
                 allowClear: true,
                 templateResult: formatTag,
